f0eead27885cb5b53190304784c2dce2e4729088
elvie <elvie@Valis>
2023-04-05 17:26:28 -0400

    Implemented shadow mechanic sketch

--- World/Shadow.gd
+++ World/Shadow.gd
@@ -7,13 +7,13 @@ var movementCounter = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(50):
-		var x = randfn(0, 1)
-		var y = randfn(0, 1)
-		movementArray.append(Vector2(x, y))
+		movementArray.append(Vector2(0, 0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mov = movementArray[movementCounter]
-	velocity = mov.normalized()*movement_speed
-	move_and_slide() 
+	position += mov
+	movementCounter+=1
+	movementCounter = movementCounter%movementArray.size()
+
--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var movement_speed = 40.0
-
+var movement_array = []
 
 
 const SPEED = 300.0
@@ -17,16 +17,25 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		var shadw = get_tree().get_first_node_in_group("Shadow")
+		shadw.global_position = global_position
+		shadw.movementArray = movement_array
+		shadw.movementCounter = 0
+		
+	
 	
 func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov, y_mov)
+	movement_array.append(mov)
 	velocity = mov.normalized()*movement_speed
 	move_and_slide() 
