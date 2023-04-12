30e0229b29a21a96e014abc9e16bafa7a43c18c4
henriquegiabardo <henriquegiabardo@usp.br>
2023-04-12 14:53:48 -0400

    added enemy moovement

--- player.gd
+++ player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction: 
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
