6a1e1a4a91c3f975c6daa711cc90210006b2b665
tinSegur <martin.segur@ug.uchile.cl>
2023-05-11 23:43:49 -0400

    Added parameters to Projectile, started on generic projectile spawner

--- Attacks/Projectiles/Projectile.gd
+++ Attacks/Projectiles/Projectile.gd
@@ -1,11 +1,16 @@
 extends Resource
 class_name Projectile
 
-# Mechanic handling params
-@export var scene : PackedScene
-@export var name: String
-@export_multiline var description: String
+# Spawner params
 @export var max_level: int
+@export var base_ammo: int
+@export var reload_speed: int
+@export var fire_rate: int
+
+# Bullet mechanic handling params
+@export var scene : PackedScene
+
+
 @export var level: int
 @export_enum("fire", "ice", "lightning") var element: int
 @export_enum("simple", "homing", "fixed") var type: int
@@ -15,12 +20,18 @@ class_name Projectile
 @export var knockback:= 100
 @export var attack_size:= 1.0
 @export var damage:= 5
+@export var flight_time := 10
 
 # Sprite handling params
 @export var sprite : Texture2D
 @export var hframes := 10
 @export var vframes := 1
 @export var rotation := 0
+
+#flavor params
+@export var name: String
+@export_multiline var description: String
+
 
 
 func level_up():
