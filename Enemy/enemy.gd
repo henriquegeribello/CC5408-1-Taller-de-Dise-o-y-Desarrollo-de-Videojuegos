6c435a4ad2d37e5a01c68720fb15786aaed2b48f
Nicolás Gallardo <nicolas.gallardo@ug.uchile.cl>
2023-06-14 11:46:19 -0400

    los esqueletos caminan

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -6,13 +6,13 @@ class_name Enemy
 @export var hp = 1
 
 @onready var player = get_tree().get_first_node_in_group("player") 
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed
 	move_and_slide()
-	
-
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
