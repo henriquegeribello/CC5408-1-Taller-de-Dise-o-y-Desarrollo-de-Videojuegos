9346279b73bf69dddf1bc98be93e49089e092314
Nicolás Gallardo <nicolas.gallardo@ug.uchile.cl>
2023-07-15 19:55:03 -0400

    Se agrega condición para ganar, según la cantidad de enemigos que mate

---
+++ win_menu.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+@onready var main_menu = $PanelContainer/MarginContainer/VBoxContainer/MainMenu
+@onready var retry = $PanelContainer/MarginContainer/VBoxContainer/Retry
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	retry.pressed.connect(_on_retry_pressed)
+	main_menu.pressed.connect(_on_main_menu_pressed)
+	hide()# Replace with function body.
+
+func show_menu():
+	show()
+	get_tree().paused = true
+
+func _on_retry_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+func _on_main_menu_pressed():
+#	get_tree().change_scene_to_packed(main_menu_scene)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().paused = false
