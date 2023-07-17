0d3a190686a39188e1946a9f7ff3214705788e44
Nicolás Gallardo <nicolas.gallardo@ug.uchile.cl>
2023-07-17 00:12:29 -0400

    se agrega animacion de muerte a enemigos

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -14,23 +14,36 @@ class_name Enemy
 @onready var playback = animation_tree.get("parameters/playback")
 var prev_pos = [0,0]
 func _physics_process(_delta):
+	
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed
+	if hp < 0:
+		velocity = Vector2(0,0)
 	move_and_slide()
 	
 	#scale.x = abs(scale.x) * sign(direction.x)
-
+var is_dead= false
 func _on_hurt_box_hurt(damage):
+	
 	hp -= damage
 	playback.travel("take hit")
 	if hp <= 0:
+		#if not is_dead:
+		playback.travel("death")
+		#	is_dead=true
 		EnemySpawner.number_of_enemies_in_la_pantalla-=1
 		EnemySpawner.number_of_enemies_slayed_by_the_player+=1
 		
 		player.change_deadEnemiesCounter(EnemySpawner.number_of_enemies_slayed_by_the_player)
 		player.add_xp()
 		
+		#_on_animation_player_animation_finished("death")
+		
+func _on_animation_player_animation_finished(anim_name):
+	# Replace with function body.
+	if anim_name == "death":
+		print("ola")#queue_free()
 
-		queue_free()
-
-
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == "death":
+		queue_free() # Replace with function body.
