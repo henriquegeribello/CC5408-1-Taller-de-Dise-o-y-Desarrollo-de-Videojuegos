c4f3f7c5a24741686511d5ea08618b8992a86fad
Nicolás Gallardo <nicolas.gallardo@ug.uchile.cl>
2023-07-16 21:11:48 -0400

    Se agrega menú de créditos

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,6 +2,8 @@ extends MarginContainer
 
 @onready var start = %Start
 @onready var exit = %Exit
+@onready var credits = %Credits
+@onready var back = $PanelContainer/MarginContainer/VBoxContainer/MarginContainer2/Back
 
 @export var main_scene: PackedScene
 
@@ -9,6 +11,7 @@ extends MarginContainer
 func _ready():
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
+	credits.pressed.connect(_on_credits_pressed)
 	
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(main_scene)
@@ -16,3 +19,5 @@ func _on_start_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://credits.tscn")
